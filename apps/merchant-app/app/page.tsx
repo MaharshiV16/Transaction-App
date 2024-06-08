3cda5d968d2277cb6d31467fef44de556e9401e4
Maharshi Vashistha <maharshivashistha@Maharshis-MacBook-Air.local>
2024-06-08 17:09:20 +0530

    Project Initialised

---
+++ apps/merchant-app/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Button } from "@repo/ui/button";
+
+export default function Home() {
+	return (
+		<div className='flex justify-center h-screen items-center'>
+			<div>Hello World</div>
+		</div>
+	);
+}
